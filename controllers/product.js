81072aab431d8068f081596c72a7ad40620acaf1
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-09-13 12:39:47 -0400

    Added get all route for controller

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,13 @@ const productApi = require('../models/product.js')
 
 const productRouter = express.Router()
 
+productRouter.route('/')
+  .get( (req, res) => {
+    productApi.getAllProducts().then(
+      products => res.send(products)
+    );
+  })
+
 // templateRouter.get('/', (req, res) => {
 //   res.send(templateApi.getHelloWorldString())
 // })
